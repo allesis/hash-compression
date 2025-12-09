77a45d97a19c3a612bd092a3a5182d30f68a2fc6
Sean Allesina-McGrory <allesis@wwu.edu>
2025-12-08 16:49:03 -0800

    Add nix-flake and supporting files
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Flake for installing dependencies needed to use wrapper script";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          lua
+        ];
+        packages = with pkgs; [
+          lux-cli
+          lua-language-server
+          stylua
+        ];
+      };
+    });
+  };
+}
