4016068bd6097a67965043afab8174c5c373038e
Sean Allesina-McGrory <allesis@wwu.edu>
2026-01-28 08:02:29 -0800

    Update nix flake
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- flake.nix
+++ flake.nix
@@ -3,46 +3,131 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    luasmith = {
+      url = "github:allesis/luasmith-flake";
+    };
+    vqmetric-nix = {
+      url = "github:allesis/vqmetric-nix?rev=18f95c4254eefc738c65d1c2e823151da462cd26";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    rust-overlay,
+    luasmith,
+    vqmetric-nix,
+    ...
   }: let
     supportedSystems = ["x86_64-linux" "x86_64-darwin"];
+    l = nixpkgs.lib // builtins;
     forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        });
+      l.genAttrs supportedSystems
+      (system:
+        f system (import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default self.overlays.default];
+        }));
   in {
-    devShells = forEachSupportedSystem ({pkgs}: let
-      lua_with_lux = pkgs.lua5_1.withPackages (ps: [
-        ps.lux-lua
-      ]);
-    in {
+    overlays.default = final: prev: {
+      rustToolchain = let
+        rust = prev.rust-bin;
+      in
+        if builtins.pathExists ./rust-toolchain.toml
+        then rust.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain
+        then rust.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust.nightly.latest.default.override {
+            extensions = ["rust-src" "rustfmt"];
+          };
+    };
+    devShells = forEachSupportedSystem (system: pkgs: {
       default = pkgs.mkShell {
-        nativeBuildInputs = [
-          lua_with_lux
-          pkgs.lux-cli
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+          rust-analyzer
+          nasm
+          libGL
+          libz
+          glibc
+          glib
+          libvmaf
+          ffmpeg-full
+          opencv
+          cargo
+          rustc
+          pkg-configUpstream
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-configUpstream
+          python313
+          python313Packages.scikit-misc
+          python313Packages.ffmpy
+          libGL
+          libz
+          glibc
+          glib
+          libvmaf
+          ffmpeg-full
+          opencv
+          cargo
+          rustc
+          jq
+          bc
+          vqmetric-nix.packages.${system}.default
+        ];
+
+        cargoBuildFlags = [
+          "--no-default-features"
+          #"--features=binaries,ivf,y4m,serde"
         ];
 
         packages = with pkgs; [
+pkg-configUpstream
+          rustToolchain
+          openssl
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+          bacon
+          clippy
+          taplo
+          lazygit
+          lua5_2
+          lua52Packages.lux-lua
           lux-cli
-	  lua_with_lux
           # lua-language-server
           emmylua-check
           emmylua-ls
           emmylua-doc-cli
-          emmy-lua-code-style
           stylua
-	  uv
-	  python314
-          python313Packages.jedi-language-server
-	  just-lsp
+          bc
+          jq
+          uv
+          libGL
+          libz
+          glibc
+          glib
+          libvmaf
+          ffmpeg-full
+          opencv
+          just-lsp
+          luasmith.packages.${system}.default
+          vqmetric-nix.packages.${system}.default
         ];
+        shellHook = ''
+          export PATH=$HOME/.cargo/bin:$PATH
+          rustup default stable
+          cargo install av-metrics-tool
+        '';
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/:${pkgs.libz}/lib/:${pkgs.libGL}/lib/:${pkgs.glib.out}/lib/:${pkgs.glibc}/lib/";
       };
     });
   };
